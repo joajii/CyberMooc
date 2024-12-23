39159b92ac5198a359830271d675f3dfea04a3bb
joajii <joakim.jaulimo@gmail.com>
2024-12-23 12:17:11 +0200

    refining the app, works as intended

--- cybermoocsite/cyberapp/views.py
+++ cybermoocsite/cyberapp/views.py
@@ -10,8 +10,9 @@ def index(request):
 def admin_access(request):
     if request.method == 'GET':
         return render(request, 'admin.html')
-    if 'admin' in request.GET:
-        return HttpResponse("Welcome, Admin!")
+    if 'admin' in request.POST:
+        if request.session.get('is_admin'):
+            return redirect('admin_dashboard')
     return HttpResponse("Access Denied.")
 
 def login_view(request):
@@ -20,9 +21,14 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.raw(f"SELECT * FROM core_user WHERE username = '{username}' AND password = '{password}'")
+        user = list(User.objects.raw(f"SELECT * FROM cyberapp_user WHERE username = '{username}' AND password = '{password}'"))
         if user:
-            return render(request, 'login.html')
+            request.session['user_id'] = user[0].id
+            request.session['is_admin'] = user[0].is_admin
+            if user[0].is_admin:
+                return redirect('admin_dashboard')
+            return redirect('user_dashboard')
+        return HttpResponse("Invalid credentials.")
 
 def register_view(request):
     if request.method == 'GET':
@@ -30,9 +36,9 @@ def register_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        User.objects.create(username=username, password=password)
+        is_admin = request.POST.get('is_admin', 'off') == 'on'
+        User.objects.create(username=username, password=password, is_admin=is_admin)
         return HttpResponse("User registered.")
-    return HttpResponse("Register page")
 
 def unvalidated_redirect(request):
     url = request.GET.get('next')
@@ -40,10 +46,10 @@ def unvalidated_redirect(request):
 
 def admin_dashboard(request):
     if request.session.get('is_admin'):
-        return render(request, 'admin_dashboard.html')
+        return render(request, 'admin-dashboard.html')
     return HttpResponse("Unauthorized access.")
 
 def user_dashboard(request):
     if request.session.get('user_id'):
-        return render(request, 'user_dashboard.html')
+        return render(request, 'user-dashboard.html')
     return HttpResponse("Unauthorized access.")
